344150c339fac7489eda878b80efeb842a8fadab
erikarezende <148558059+erikarezende@users.noreply.github.com>
2024-11-14 09:59:32 -0300

    Update testegithub.py

--- testegithub.py
+++ testegithub.py
@@ -58,3 +58,16 @@ st.write('Você selecionou: ', opcao)
 
 dadosFiltrados = df[df['nomeServidor'] == opcao]
 st.write(dadosFiltrados)
+
+#testando páginas EXTRAA
+
+import streamlit as st
+
+def page2():
+    st.title("Second page")
+
+pg = st.navigation([
+    st.Page("page1.py", title="First page", icon="🔥"),
+    st.Page(page2, title="Second page", icon=":material/favorite:"),
+])
+pg.run()
