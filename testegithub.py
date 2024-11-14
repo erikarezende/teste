094ceb81e18a6ca3cdbc824b905dd1e4bf6eafed
erikarezende <148558059+erikarezende@users.noreply.github.com>
2024-11-14 09:43:30 -0300

    Update testegithub.py

--- testegithub.py
+++ testegithub.py
@@ -37,3 +37,22 @@ st.success("Você conseguiu!")
 st.error("Erro!")
 st.warning("Advertência")
 st.info("Esta é uma informação")
+
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'nomeServidor': ['Adriana', 'Monica', 'Samara'],
+    'salario': [1200,300,5000]
+})
+
+st.write("Criando uma tabela!")
+st.write(df)
+opcao = st.selectbox(
+    'Qual servidor você gostaria de selecionar?',
+     df['nomeServidor'])
+
+st.write('Você selecionou: ', opcao)
+
+dadosFiltrados = df[df['nomeServidor'] == opcao]
+st.write(dadosFiltrados)
