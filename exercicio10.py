da2bce0faf2fddde35402949187abd1d1c24b9cb
erikarezende <148558059+erikarezende@users.noreply.github.com>
2024-11-17 17:35:41 -0300

    Update exercicio10.py

--- exercicio10.py
+++ exercicio10.py
@@ -1,4 +1,55 @@
-import seaborn as sns
+import requests
+import pandas as pd
+!pip install streamlit
+import streamlit as st
+
+url='https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=F&ordem=ASC&ordenarPor=nome'
+
+response = requests.get(url).json()
+
+response.keys()
+
+#acessando a chave dados do dicionário
+dfmulheres = pd.DataFrame(response['dados'])
+
+#contando a quantidade de uf de deputadas
+uf_counts = dfmulheres['siglaUf'].value_counts()
+
+# Transformando a Series em um DataFrame
+df_uf_counts = uf_counts.reset_index()
+
+# Renomeando as colunas para tornar mais claro
+df_uf_counts.columns = ['Estado', 'Quantidade_de_Mulheres']
+
+# Exibindo o DataFrame
+print(df_uf_counts)
+
+url2='https://dadosabertos.camara.leg.br/api/v2/deputados?siglaSexo=M&ordem=ASC&ordenarPor=nome'
+
+response_h = requests.get(url2).json()
+
+#acessando a chave 'dados' do dicionário e criando o dataframe
+dfhomens = pd.DataFrame(response_h['dados'])
+
+#contando a quantidade de uf de deputados
+uf_counts_h = dfhomens['siglaUf'].value_counts()
+
+# Transformando a Series em um DataFrame
+df_uf_counts_h = uf_counts_h.reset_index()
+
+# Renomeando as colunas para tornar mais claro
+df_uf_counts_h.columns = ['Estado', 'Quantidade_de_Homens']
+
+# Exibindo o DataFrame
+print(df_uf_counts_h)
+
+df_total = pd.merge(df_uf_counts, df_uf_counts_h, on='Estado', how='inner')
+
+df_total
+
+#adicionando a coluna total de deputados
+
+df_total['Total_de_deputados'] = df_total['Quantidade_de_Mulheres'] + df_total['Quantidade_de_Homens']
 
 # Criando o gráfico com Seaborn (gráfico de barras)
 df_total_melted = df_total.melt(id_vars='Estado', value_vars=['Quantidade_de_Homens', 'Quantidade_de_Mulheres'], var_name='Sexo', value_name='Quantidade')
@@ -16,4 +67,5 @@ plt.ylabel('Quantidade de Deputados')
 plt.tight_layout()
 plt.show()
 
-st.pyplot(plt)
+st.bar_chart(df_total)
+             
